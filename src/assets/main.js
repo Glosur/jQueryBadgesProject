852b831a01790b3f00bbc0b242c1480a662a411d
Sylvain Blanc <s.blanc@hotmail.fr>
2017-01-31 17:50:08 +0100

    Task 8_9

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,8 +5,8 @@ $(function() {
     dataType: 'jsonp',
     success: function(response) {
      response.courses.completed.forEach(function(course)  {
-      	var badge = $('#badges').append(`<div class="course"><h3>${course.title}</h3></div>`);
-      	console.log(course);
+      	var badge = $('#badges').append(`<div class="course"><h3>${course.title}</h3><img src=${course.badge}></img><a href=${course.url} target='_blank' class='btn btn-primary'>See Course</a></div>`);
+      	//console.log(course);
       	//badge.find('div').prepend('<h3>' + course.title + '</h3>');
       });
     }
