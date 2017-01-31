238f1ba845892034b996a49952a7e8836e00c9ec
Sylvain Blanc <s.blanc@hotmail.fr>
2017-01-31 17:46:04 +0100

    Task 6-7 + foreach

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,9 +4,11 @@ $(function() {
     url: 'https://www.codeschool.com/users/Glosur.json',
     dataType: 'jsonp',
     success: function(response) {
-      for (course in response.courses.completed) {
-      	$('#badges').append('<div class="course"></div>');
-      }
+     response.courses.completed.forEach(function(course)  {
+      	var badge = $('#badges').append(`<div class="course"><h3>${course.title}</h3></div>`);
+      	console.log(course);
+      	//badge.find('div').prepend('<h3>' + course.title + '</h3>');
+      });
     }
   });
 
