0488b6a944735ec34ecf742197550297f8e6f328
Sylvain Blanc <s.blanc@hotmail.fr>
2017-01-31 17:35:28 +0100

    Task 4-5

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Glosur.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for (course in response.courses.completed) {
+      	$('#badges').append('<div class="course"></div>');
+      }
+    }
+  });
 
 });
